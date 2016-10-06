010431170b9b803a9f9a19c05d63b3f2ee3b0881
Gabi Procell <gprocell927@gmail.com>
2016-10-06 10:34:17 -0600

    WIP feature testing for input values

--- test/specs/test.js
+++ test/specs/test.js
@@ -7,3 +7,34 @@ describe('welcome page', function() {
       assert.equal(title, 'ToDo Box');
   });
 });
+
+describe('attributes on our application', function (){
+  it('has input forms and I can set values in those forms', function() {
+    browser.url('/');
+    var todoTitle = browser.element("#title-input");
+    var todoDescription = browser.element("#body-input");
+
+    todoTitle.setValue('great title');
+    todoDescription.setValue('great description');
+
+    assert.equal(todoTitle.getValue(), 'great title');
+    assert.equal(todoDescription.getValue(), 'great description');
+  });
+
+  it('should be able to add my todos to the page', function(){
+    browser.url('/');
+    var todoTitle = browser.element("#title-input");
+    var todoDescription = browser.element("#body-input");
+
+    todoTitle.setValue('great title');
+    todoDescription.setValue('great description');
+
+    assert.equal(todoTitle.getValue(), 'great title');
+    assert.equal(todoDescription.getValue(), 'great description');
+
+    browser.click('#save-button');
+
+    var allTodos = browser.getText('li');
+    assert.equal(allTodos.replace(/\n/g, ", "), 'great title, great description');
+  });
+});
