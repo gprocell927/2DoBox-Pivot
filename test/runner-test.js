2b0a21507afd783d2bb396864cb2417516b4172a
Gabi Procell <gprocell927@gmail.com>
2016-10-08 11:58:06 -0600

    WIP runner test file linked, unit tests running

--- test/runner-test.js
+++ test/runner-test.js
@@ -1,10 +1,20 @@
 const assert = require('chai').assert;
 const Runner = require('../lib/runner.js');
-const Idea = require('../lib/idea.js');
 
 describe('Runner', function() {
-  context('it should have an array that stores ideas', function(){
+
+  it('should have an array that stores ideas', function(){
     var runner = new Runner();
-    assert.isArray(runner.ideasList, true);
+    assert.isArray(runner.ideasList);
   });
+
+  it('should create an Idea when "generateNewIdea()" is called', function(){
+    var runner = new Runner();
+
+    runner.generateNewIdea();
+
+    assert.equal(runner.ideasList.length, 1);
+    assert.isObject(runner.ideasList[0]);
+  });
+
 });
